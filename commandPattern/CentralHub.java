97cb7d982f0f0b07ae8c9b780d15d67b6f71eeaf
Kim Aeriel V. Fonseca <kimmyaeriel@gmail.com>
2026-09-03 17:11:46 +0800

    Update CentralHub.java

--- commandPattern/CentralHub.java
+++ commandPattern/CentralHub.java
@@ -4,9 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CentralHub {
-    // The hub only ever talks to the Command interface.
-    // It never knows what kind of device is behind a command,
-    // so new devices can be added without changing this class.
     private Map<String, Command> commands = new HashMap<>();
 
     public void setCommand(String label, Command command) {
