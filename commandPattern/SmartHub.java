8373fa0d671a73ed6c18e65d79b61a3a3e1268ac
Kim Aeriel V. Fonseca <kimmyaeriel@gmail.com>
2026-09-03 17:38:40 +0800

    Update and rename CentralHub.java to SmartHub.java

--- commandPattern/SmartHub.java
+++ commandPattern/SmartHub.java
@@ -3,7 +3,7 @@ package commandPattern;
 import java.util.HashMap;
 import java.util.Map;
 
-public class smartHub{
+public class SmartHub{
     private Map<String, Command> commands = new HashMap<>();
 
     public void setCommand(String label, Command command) {
